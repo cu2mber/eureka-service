89db75ce793d7404d416b464f3a4687eb17f904d
junyong park <pjy37414@gmail.com>
2025-09-17 20:01:17 +0900

    build: 초기 Maven + Spring Boot 프로젝트 구조 생성, src, pom.xml, gitignore, gitattributes, Maven Wrapper

---
+++ src/test/java/com/cu2mber/eurekaservice/EurekaServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cu2mber.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
